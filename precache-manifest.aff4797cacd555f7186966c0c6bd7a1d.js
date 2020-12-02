925063aeba4804c204394a8f4838062bcd298a7c
shalala0115 <shalala0115@gmail.com>
2020-12-02 08:11:19 -0800

    Updates

---
+++ precache-manifest.aff4797cacd555f7186966c0c6bd7a1d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c7476e6599c5a5d311bbcd333c8e9e77",
+    "url": "/todolist/index.html"
+  },
+  {
+    "revision": "b0161de861d315b69806",
+    "url": "/todolist/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "3ea81d82953b3c81b82c",
+    "url": "/todolist/static/js/2.d2fba637.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/todolist/static/js/2.d2fba637.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b0161de861d315b69806",
+    "url": "/todolist/static/js/main.4b6eba64.chunk.js"
+  },
+  {
+    "revision": "bae3b32ef02ac922504d",
+    "url": "/todolist/static/js/runtime-main.7a8f4f2b.js"
+  }
+]);
